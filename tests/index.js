6b2466374f5b7348d05adcf526690bfda46f569d
Anabella Spinelli <anabellaspinelli@gmail.com>
2017-07-11 17:37:03 -0300

    Added testReport mocha opts file.

--- tests/index.js
+++
@@ -1,6 +0,0 @@
-global.fs = require('fs');
-global.path = require('path');
-let request = require('supertest');
-global.chai = require('chai');
-
-global.typeformDataAPI = request('https://api.typeform.com/v1');
